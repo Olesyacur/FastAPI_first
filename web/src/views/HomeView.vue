b020235ed7c585aca950ead357b5ab24c88c497c
Olesya <olesikand@mail.ru>
2022-11-22 23:18:46 +0300

    реактивность с vue

---
+++ web/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Я, {{ name }} <span id="curDT"></span>,обещаю бросить курить в 45 лет.
+    <br>
+    _______________/{{ name }}/
+  </div>
+  <div style="margin-top: 20px;">
+    <input type="text" v-model="name">
+    <input type="submit" value="Установить имя">
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('')
+</script>
